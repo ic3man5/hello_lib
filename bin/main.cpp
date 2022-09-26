6f0a9015ddeb51de2656b8020aac554b98a353ff
David Rebbe <1187684+ic3man5@users.noreply.github.com>
2022-09-26 15:01:13 -0400

    Added one more function

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,5 +4,14 @@
 int main(int argc, char* argv[])
 {
     std::cout << hello() << "\n";
+
+    auto hello_struct = hello_advanced();
+    std::cout << "Hello from struct buffer: " << hello_struct->buffer << "\n";
+    std::cout << "Is hello: " << hello_struct->is_hello << "\n";
+
+    // We have to manually free the memory here or its a memory leak.
+    free((void*)hello_struct);
+    hello_struct = nullptr;
+    
     return 0;
 }
